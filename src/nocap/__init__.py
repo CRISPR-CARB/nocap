22a3d4586bf4ba68264b80a5be711fc926faf1bd
August <30163079+augeorge@users.noreply.github.com>
2024-08-20 17:05:01 -0700

    update __init__.py

--- src/nocap/__init__.py
+++ src/nocap/__init__.py
@@ -19,8 +19,10 @@ from .scm import (
     read_dag_file,
     regress_lscm,
 )
+from .utility import convert_grn_dag_to_sbml
 
 __all__ = [
+    "convert_grn_dag_to_sbml",
     "convert_to_eqn_array_latex",
     "convert_to_latex",
     "dagitty_to_digraph",
